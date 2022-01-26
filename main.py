3d98e3a64c06ba29c73e07091997d1efe484af37
patrickhluo <Tomohiroluo@gmail.com>
2022-01-26 13:16:15 -0500

    remove unwanted libraries

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ import time
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-import scipy
 from PIL import Image
-from scipy import ndimage
 from dnn_app_utils_v3 import *
 
-# %matplotlib inline
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-# %load_ext autoreload
-# %autoreload 2
 
 np.random.seed(1)
 
@@ -54,7 +49,7 @@ n_h = 7
 n_y = 1
 layers_dims = (n_x, n_h, n_y)
 
-# GRADED FUNCTION: two_layer_model
+# two_layer_model
 
 def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
     """
@@ -79,9 +74,7 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
     (n_x, n_h, n_y) = layers_dims
     
     # Initialize parameters dictionary, by calling one of the functions you'd previously implemented
-    ### START CODE HERE ### (≈ 1 line of code)
     parameters = initialize_parameters(n_x, n_h, n_y)
-    ### END CODE HERE ###
     
     # Get W1, b1, W2 and b2 from the dictionary parameters.
     W1 = parameters["W1"]
@@ -94,24 +87,18 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
     for i in range(0, num_iterations):
 
         # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1, W2, b2". Output: "A1, cache1, A2, cache2".
-        ### START CODE HERE ### (≈ 2 lines of code)
         A1, cache1 = linear_activation_forward(X, W1, b1, activation="relu")
         A2, cache2 = linear_activation_forward(A1, W2, b2, activation="sigmoid")
-        ### END CODE HERE ###
         
         # Compute cost
-        ### START CODE HERE ### (≈ 1 line of code)
         cost = compute_cost(A2, Y)
-        ### END CODE HERE ###
         
         # Initializing backward propagation
         dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
         
         # Backward propagation. Inputs: "dA2, cache2, cache1". Outputs: "dA1, dW2, db2; also dA0 (not used), dW1, db1".
-        ### START CODE HERE ### (≈ 2 lines of code)
         dA1, dW2, db2 = linear_activation_backward(dA2, cache2, activation="sigmoid")
         dA0, dW1, db1 = linear_activation_backward(dA1, cache1, activation="relu")
-        ### END CODE HERE ###
         
         # Set grads['dWl'] to dW1, grads['db1'] to db1, grads['dW2'] to dW2, grads['db2'] to db2
         grads['dW1'] = dW1
@@ -120,9 +107,7 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
         grads['db2'] = db2
         
         # Update parameters.
-        ### START CODE HERE ### (approx. 1 line of code)
         parameters = update_parameters(parameters, grads, learning_rate)
-        ### END CODE HERE ###
 
         # Retrieve W1, b1, W2, b2 from parameters
         W1 = parameters["W1"]
@@ -149,7 +134,7 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
 ### CONSTANTS ###
 layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
 
-# GRADED FUNCTION: L_layer_model
+# L_layer_model
 
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
     """
@@ -171,32 +156,22 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     costs = []                         # keep track of cost
     
     # Parameters initialization. (≈ 1 line of code)
-    ### START CODE HERE ###
     parameters = initialize_parameters_deep(layers_dims)
-    ### END CODE HERE ###
     
     # Loop (gradient descent)
     for i in range(0, num_iterations):
 
         # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        ### START CODE HERE ### (≈ 1 line of code)
         AL, caches = L_model_forward(X, parameters)
-        ### END CODE HERE ###
         
         # Compute cost.
-        ### START CODE HERE ### (≈ 1 line of code)
         cost = compute_cost(AL, Y)
-        ### END CODE HERE ###
     
         # Backward propagation.
-        ### START CODE HERE ### (≈ 1 line of code)
         grads = L_model_backward(AL, Y, caches)
-        ### END CODE HERE ###
  
         # Update parameters.
-        ### START CODE HERE ### (≈ 1 line of code)
         parameters = update_parameters(parameters, grads, learning_rate)
-        ### END CODE HERE ###
                 
         # Print the cost every 100 training example
         if print_cost and i % 100 == 0:
